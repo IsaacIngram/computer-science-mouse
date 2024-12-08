0b32dadd7af98840a83bc61144028e093fea9caf
IsaacIngram <me@isaacingram.dev>
2024-12-07 23:44:51 -0500

    Add ESP-NOW functionality

--- trap/main/trap.c
+++ trap/main/trap.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <driver/gpio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
+#include "esp_now.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 // Pin definitions
 #define HAMMER_SENSOR_PIN 23
 #define LED_PIN 2
 
 #define HAMMER_POLL_DELAY_MS 500
+
+uint8_t peer_mac[ESP_NOW_ETH_ALEN] = {0x94, 0xB5, 0x55, 0x8E, 0x2A, 0x20};
 
 /**
  * Configure pins
@@ -18,7 +25,7 @@ void configure_pins() {
             .mode = GPIO_MODE_INPUT,
             .pin_bit_mask = 1ULL << HAMMER_SENSOR_PIN,
             .pull_up_en = GPIO_PULLUP_DISABLE,
-            .pull_down_en = GPIO_PULLDOWN_DISABLE
+            .pull_down_en = GPIO_PULLDOWN_ENABLE
     };
     gpio_config(&hammer_sensor_conf);
     // LED pin
@@ -40,6 +47,24 @@ bool is_hammer_down() {
     return gpio_get_level(HAMMER_SENSOR_PIN);
 }
 
+
+void send_status_esp_now(bool status) {
+    esp_now_peer_info_t peer_info = {
+            .channel = 0,
+            .ifidx = ESP_IF_WIFI_STA,
+            .encrypt = false,
+    };
+    memcpy(peer_info.peer_addr, peer_mac, ESP_NOW_ETH_ALEN);
+
+    // Add peer if not already added
+    if (!esp_now_is_peer_exist(peer_mac)) {
+        esp_now_add_peer(&peer_info);
+    }
+    esp_now_send(peer_mac, (uint8_t*)&status, sizeof(status));
+    printf("Sent ESP-NOW message.\n");
+}
+
+
 /**
  * Task to repeatedly check the hammer status and set LEDs accordingly
  * @param pvParameters
@@ -48,11 +73,36 @@ _Noreturn void check_hammer_task(void *pvParameters) {
     while(1) {
         if (is_hammer_down()) {
             gpio_set_level(LED_PIN, 1);
+            send_status_esp_now(true);
         } else {
             gpio_set_level(LED_PIN, 0);
+            send_status_esp_now(false);
         }
-        vTaskDelay(pdMS_TO_TICKS(HAMMER_POLL_DELAY_MS));
     }
+}
+
+void init_esp_now() {
+    esp_err_t ret;
+
+    // Initialize NVS
+    ret = nvs_flash_init();
+    while (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        ret = nvs_flash_init();
+    }
+    printf("Flash initialized");
+
+    // Initialize Wi-Fi
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    printf("WiFi initialized\n");
+
+    // Initialize ESP-NOW
+    ESP_ERROR_CHECK(esp_now_init());
 }
 
 
@@ -60,6 +110,8 @@ void app_main(void)
 {
     // Configure all GPIO pins
     configure_pins();
+
+    init_esp_now();
 
     // Start hammer task
     xTaskCreate(&check_hammer_task, "check_hammer_task", 2048, NULL, 5, NULL);
